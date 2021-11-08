9518130902c78098157cb6d7f935c90bb3eb0a11
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-08 17:00:03 +0300

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,19 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            
+            <div className='header_left'>
+
+            </div>
+
+            <div className='header_middle'>
+
+            </div>
+
+            <div className='header_right'>
+
+            </div>
         </div>
-    );
+    ); 
 }
 
 export default Header;
