68a2f239b1bfb521057316388ca82158a4560ea8
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-08 16:37:07 +0300

    facebook clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDgLZr5slE-GFww3eEras8tQ3qaYi6OvtU",
+    authDomain: "facebook-clone-734f8.firebaseapp.com",
+    projectId: "facebook-clone-734f8",
+    storageBucket: "facebook-clone-734f8.appspot.com",
+    messagingSenderId: "961805235147",
+    appId: "1:961805235147:web:3d3ac856a0fa0b83fb3c4e",
+    measurementId: "G-FVZKFKR1WF"
+  };
