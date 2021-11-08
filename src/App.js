42a1625eaa1641acff02a1399221dcca08455ada
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-08 19:26:58 +0300

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <div className='app'>
       
       {/*Header */}
+<Header />
 
       {/*App Body */}
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.css';
+import logo from '../Media/download.png';
 
 const Header = () => {
     return (
         <div className='header'>
             <div className='header_left'>
-
+<img src={logo} alt='logo' />
             </div>
 
             <div className='header_middle'>
