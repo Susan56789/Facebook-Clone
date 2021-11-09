b439ac2d1ad5f6d5d5a3dbf91657eebed2b8b7f6
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-09 19:58:40 +0300

    messege sender

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Feed.css';
 import StoryReel from './StoryReel';
-
+import MessageSender from './MessageSender';
 
 const Feed = () => {
     return (
         <div className='feed'>
             {/*Story Reel */}
               <StoryReel />
-              
-            {/*Messenge Sender */}
 
+            {/*Messenge Sender */}
+              <MessageSender />
 
         </div>
     );
