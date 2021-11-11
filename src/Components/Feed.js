ebc7e2b9a1c27368b01f40c08a0a51bdc59b9d0f
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-11 11:42:03 +0300

    post

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -4,7 +4,7 @@ import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
 import Post from './Post';
 
-const Feed = () => {
+const Feed = ({profilePic, message, timestamp, username, image}) => {
     return (
         <div className='feed'>
             {/*Story Reel */}
@@ -14,9 +14,27 @@ const Feed = () => {
               <MessageSender />
 
           {/*Post */}
-          <Post />
-          <Post />
-          <Post />
+          <Post 
+          profilePic='https://images.unsplash.com/photo-1496440737103-cd596325d314?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z2lybHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          message='Beuty in nature'
+          timestamp='Timestamp'
+          username='Ann Marie'
+          image='https://images.unsplash.com/photo-1525310072745-f49212b5ac6d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          />
+          <Post 
+          profilePic='https://images.unsplash.com/photo-1484800089236-7ae8f5dffc8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxhZHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          message='Looooove'
+          timestamp='time'
+          username='Sue N'
+          image='https://images.unsplash.com/photo-1529042410759-befb1204b468?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          />
+          <Post 
+          profilePic='https://images.unsplash.com/photo-1542282088-fe8426682b8f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          message='A cup of coffee a day keeps the hangover away ...'
+          timestamp='Time'
+          username='John K.'
+          image='https://images.unsplash.com/photo-1518057111178-44a106bad636?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          />
         </div>
     );
 }
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './Post.css';
 import { Avatar } from '@material-ui/core';
 
-function Post({profilePic, image, userName, timestamp,messageSender}) {
+function Post({profilePic, image, username, timestamp,message}) {
     return (
         <div className='post'>
             <div className='post_top'>
                 <Avatar src={profilePic}  className='post_avatar'/>
                 <div className='post_topInfo'>
-            <h3>{userName}</h3>
-            <p>{new Date(timestamp?.toDate()).toUTCString() }</p>
+            <h3>{username}</h3>
+            <p>Timestamp</p>
                 </div>
             </div>
             <div className='post_bottom'>
-
+                  <p>{message}</p>
+            </div>
+            <div className='post_image'>
+                <img src={image} alt=' ' />
             </div>
         </div>
     );
