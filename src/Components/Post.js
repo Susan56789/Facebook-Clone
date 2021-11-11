8564fc88573ff7fbe4c28f6a25eaf030a77b53dc
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-11 12:06:54 +0300

    post

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Post.css';
 import { Avatar } from '@material-ui/core';
+import { AccountCircle, ChatBubbleOutline, ExpandMoreOutlined, NearMe, ThumbUpSharp } from '@material-ui/icons';
 
 function Post({profilePic, image, username, timestamp,message}) {
     return (
@@ -17,6 +18,24 @@ function Post({profilePic, image, username, timestamp,message}) {
             </div>
             <div className='post_image'>
                 <img src={image} alt=' ' />
+            </div>
+            <div className='post_options'>
+             <div className='post_option'>
+                 <ThumbUpSharp />
+                 <p>Like</p>
+             </div>
+             <div className='post_option'>
+                 <ChatBubbleOutline />
+                 <p>Comment</p>
+             </div>
+             <div className='post_option'>
+                 <NearMe />
+                 <p>Share</p>
+             </div>
+             <div className='post_option'>
+                 <AccountCircle />
+                 <ExpandMoreOutlined />
+             </div>
             </div>
         </div>
     );
