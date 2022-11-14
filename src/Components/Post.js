696e4e58b9d0622674779fb81f2765df633cd976
susan56789 <mwihaki2susan@gmail.com>
2022-11-14 15:27:56 +0300

    updates

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,39 +3,39 @@ import './Post.css';
 import { Avatar } from '@material-ui/core';
 import { AccountCircle, ChatBubbleOutline, ExpandMoreOutlined, NearMe, ThumbUpSharp } from '@material-ui/icons';
 
-function Post({profilePic, image, username, timestamp,message}) {
+function Post({ profilePic, image, username, timestamp, message }) {
     return (
         <div className='post'>
             <div className='post_top'>
-                <Avatar src={profilePic}  className='post_avatar'/>
+                <Avatar src={profilePic} className='post_avatar' />
                 <div className='post_topInfo'>
-            <h3>{username}</h3>
-            <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
+                    <h3>{username}</h3>
+                    <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
                 </div>
             </div>
             <div className='post_bottom'>
-                  <p>{message}</p>
+                <p>{message}</p>
             </div>
             <div className='post_image'>
                 <img src={image} alt=' ' />
             </div>
             <div className='post_options'>
-             <div className='post_option'>
-                 <ThumbUpSharp />
-                 <p>Like</p>
-             </div>
-             <div className='post_option'>
-                 <ChatBubbleOutline />
-                 <p>Comment</p>
-             </div>
-             <div className='post_option'>
-                 <NearMe />
-                 <p>Share</p>
-             </div>
-             <div className='post_option'>
-                 <AccountCircle />
-                 <ExpandMoreOutlined />
-             </div>
+                <div className='post_option'>
+                    <ThumbUpSharp />
+                    <p>Like</p>
+                </div>
+                <div className='post_option'>
+                    <ChatBubbleOutline />
+                    <p>Comment</p>
+                </div>
+                <div className='post_option'>
+                    <NearMe />
+                    <p>Share</p>
+                </div>
+                <div className='post_option'>
+                    <AccountCircle />
+                    <ExpandMoreOutlined />
+                </div>
             </div>
         </div>
     );
